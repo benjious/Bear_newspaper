0b91fd50b98b13405a7ee6d26a566c12e347730f
benjious <893326146@qq.com>
2017-01-08 23:16:47 +0800

    修改了OkHttoUtils ,下次记得,在Living temp中增加newInstance()

--- app/src/main/java/com/example/benjious/bear_newspaper/util/OkHttpUtils.java
+++ app/src/main/java/com/example/benjious/bear_newspaper/util/OkHttpUtils.java
@@ -1,6 +1,15 @@
 package com.example.benjious.bear_newspaper.util;
 
 
+import android.os.Handler;
+import android.os.Looper;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Benjious on 2017/1/8.
  */
@@ -8,6 +17,20 @@ package com.example.benjious.bear_newspaper.util;
 public class OkHttpUtils {
     public static final String TAG="OkHttpUtils";
     private static OkHttpUtils mInstance;
+    private OkHttpClient mOkHttpClient;
+    private Handler mDelivery;
+
+    public OkHttpUtils() {
+        mOkHttpClient=new OkHttpClient();
+        mOkHttpClient.setConnectTimeout(10, TimeUnit.SECONDS);
+        mOkHttpClient.setWriteTimeout(10, TimeUnit.SECONDS);
+        mOkHttpClient.setReadTimeout(30, TimeUnit.SECONDS);
+        //cookie enabled
+        mOkHttpClient.setCookieHandler(new CookieManager(null, CookiePolicy.ACCEPT_ORIGINAL_SERVER));
+
+        //这里定义一个handler,方便转回主线程执行操作
+        mDelivery=new Handler(Looper.getMainLooper());
+    }
 
 
 }
